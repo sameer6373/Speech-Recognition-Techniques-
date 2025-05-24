c3ae07ba93be068b1b92027fa2ddb57f9110189a
sameer6373 <sameermass002@gmail.com>
2025-05-24 22:11:56 +0530

    Create home_config.py

---
+++ Voice controlled smart homes/home_config.py
@@ -0,0 +1,6 @@
+# home_config.py
+home = {
+    "living room": {"light": False, "fan": False},
+    "bedroom": {"light": False, "ac": False},
+    "kitchen": {"light": False, "exhaust fan": False}
+}
